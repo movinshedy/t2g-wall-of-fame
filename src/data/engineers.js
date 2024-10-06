3f802eecf980ba90c7cbc78f14a3d0d8c749b4dc
codeme254 <otwomadennis365@gmail.com>
2024-10-06 14:52:27 +0300

    update engineers.js file

--- src/data/engineers.js
+++ src/data/engineers.js
@@ -1,4 +1,3 @@
-import sampleHeadshotImg from '../assets/sample-headshot.jpg';
 import mosesmukabi from '../assets/mosesmukabi.jpg'
 const engineers = [
     {
@@ -7,41 +6,6 @@ const engineers = [
         excerpt: "I am a software engineer with a BSc in Information Technology from Murang'a University. Skilled in programming languages like JavaScript and Python, I specialize in web development using technologies such as React, HTML, and CSS. I am passionate about building efficient, scalable applications and continuously improving my skills.",
         portfolioUrl: "/mosesmukabi",
         githubUrl: "https://github.com/mosesmukabi"
-    },
-    {
-        headshot: sampleHeadshotImg,
-        name: 'florah nduati',
-        excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi?",
-        portfolioUrl: "/",
-        githubUrl: "https://github.com/codeme254"
-    },
-    {
-        headshot: sampleHeadshotImg,
-        name: 'florah nduati',
-        excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi?",
-        portfolioUrl: "/",
-        githubUrl: "https://github.com/codeme254"
-    },
-    {
-        headshot: sampleHeadshotImg,
-        name: 'florah nduati',
-        excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi?",
-        portfolioUrl: "/",
-        githubUrl: "https://github.com/codeme254"
-    },
-    {
-        headshot: sampleHeadshotImg,
-        name: 'florah nduati',
-        excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi?",
-        portfolioUrl: "/",
-        githubUrl: "https://github.com/codeme254"
-    },
-    {
-        headshot: sampleHeadshotImg,
-        name: 'florah nduati',
-        excerpt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rem expedita praesentium odio doloremque soluta, quisquam voluptatem iste voluptas excepturi?",
-        portfolioUrl: "/",
-        githubUrl: "https://github.com/codeme254"
-    },
+    }
 ]
 export default engineers;
