031d382879f18cad22172492fa7b9a2c5feebcd8
Salutah <salutahmoha@gmail.com>
2024-10-06 18:12:05 +0300

    Building a hero section for my portfolio

--- src/pages/salad/components/Skills/SkillsSection.jsx
+++ src/pages/salad/components/Skills/SkillsSection.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import SkillStled from "../styled/Skills-section";
-import SkillsSectionData from "./SkillsData";
+// import React from "react";
+// import SkillStled from "../styled/Skills-section";
+// import SkillsSectionData from "./SkillsData";
 
-function SkillSection() {
-  return (
-    <SkillStled>
-      <div className="skills-container">
-        <h3>Skills</h3>
-        <div className="skills">
-          {SkillsSectionData.map((Skills, index) => (
-            <div className="skill decagon" key={index}>
-              <img src={Skills.image} alt="" />
-            </div>
-          ))}
-        </div>
-      </div>
-    </SkillStled>
-  );
-}
+// function SkillSection() {
+//   return (
+//     <SkillStled>
+//       <div className="skills-container">
+//         <h3>Skills</h3>
+//         <div className="skills">
+//           {SkillsSectionData.map((Skills, index) => (
+//             <div className="skill decagon" key={index}>
+//               <img src={Skills.image} alt="" />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </SkillStled>
+//   );
+// }
 
-export default SkillSection;
+// export default SkillSection;
